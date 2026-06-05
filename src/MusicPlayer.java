0d4ee45584f39d27ceb94372bf5744fe0f1fb49a
Elliot Scott <elliotscott2111@gmail.com>
2026-06-06 00:02:37 +0100

    Added music file to source and started developing musicPlayer()

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,6 +1,24 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
 public class MusicPlayer {
 
     static void main(String[] args){
+        musicPlayer();
+    }
 
+    public static void musicPlayer(){
+        String filePath = "Covenant Dance.wav";
+        File file = new File(filePath);
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+        }catch(IOException e){
+            IO.println("something went wrong.");
+        } catch (UnsupportedAudioFileException e) {
+            IO.println("audio file is not supported. Please use .wav, .au or .aiff");
+        }
     }
 }
